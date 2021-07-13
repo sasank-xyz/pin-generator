87cf7a93f276289ccd5999ddb6b48b28558804a2
Sasank Sasikumar <sasank@hey.com>
2021-07-13 21:13:31 +1200

    test: add test to check 1000 pins are generated

--- src/test/java/com/example/pingenerator/PinGeneratorTest.java
+++ src/test/java/com/example/pingenerator/PinGeneratorTest.java
@@ -2,10 +2,16 @@ package com.example.pingenerator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PinGeneratorTest {
+    private final PinGeneratorInterface pinGenerator = new PinGenerator();
+
     @Test
-    public void shouldReturnEmptyList() {
-        PinGeneratorInterface pinGenerator = new PinGenerator();
-        assert(pinGenerator.generatePins().isEmpty());
+    public void shouldGenerate1000Pins() {
+        Collection<String> pins = pinGenerator.generatePins();
+        assertEquals(1000, pins.size());
     }
 }
